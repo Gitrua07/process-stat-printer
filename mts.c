3496af41d17efb59f65203806fbe785017842b0f
Gaia Iturralde <gaiaiturralde@linux204.csc.uvic.ca>
2025-03-04 20:34:25 -0800

    fixed ordering in threads

--- mts.c
+++ mts.c
@@ -5,11 +5,40 @@
 #include <stdint.h>
 #include <string.h>
 
-struct train{
+struct train{ 
     char train_direction[1];
     int load_time;
     int cross_time;
 };
+
+int queue[75]; /* Line 14 - 41: Helped by ChatGPT */
+int head = 0, tail = 0;
+
+int isEmpty() {
+    return head == tail;
+}
+
+int isFull() {
+    return (tail + 1) % 75 == head;
+}
+
+void enqueue(int train_index) {
+    if (isFull()) {
+        printf("Queue is full! Cannot enqueue train %d.\n", train_index);
+        return;
+    }
+    queue[tail] = train_index;
+    tail = (tail + 1) % 75;
+}
+
+int dequeue() {
+    if (isEmpty()) {
+        return -1;  // Queue is empty
+    }
+    int train = queue[head];
+    head = (head + 1) % 75;
+    return train;
+}
 
 struct train train_data[75]; /* Holds train attributes */
 pthread_mutex_t count_mutex; /* Holds mutex */
@@ -20,7 +49,7 @@ pthread_cond_t count_cond2;
 int load_done = 0; /* Checks if loading time is done. 0: Not done, 1: done */
 int load_done2 = 0;
 int train_index = 0;
-char *direction;
+//double curr_time = 0;
 
 void *loading_time(void *index){ /* Add locks and conditions here*/
     /*
@@ -30,43 +59,31 @@ void *loading_time(void *index){ /* Add locks and conditions here*/
      *     index:  index: the number of the train to load
      *
      */
+     char *train_direction_l = train_data[(intptr_t)index].train_direction;
+     int load_time_l = train_data[(intptr_t)index].load_time;
+     int index_l = (intptr_t)index;
 
-     double new_time = ((double)train_data[(intptr_t)index].load_time)/10;
+     double new_time = (double)(load_time_l)/10;
      useconds_t load_time2 = (useconds_t)(new_time * 1000000);
-
      usleep(load_time2); /* Loads time */
-
-     pthread_mutex_lock(&count_mutex); /* Locks code below */
+     
      printf("00:00:0%.1f ", new_time);
      
-     if(strcmp(train_data[(intptr_t)index].train_direction, "W")==0 || strcmp(train_data[(intptr_t)index].train_direction, "w")==0){
-         printf("Train %2ld is ready to go West\n", (intptr_t)index);
-         direction = "West";
+     char *direction;
+     if(strcmp(train_direction_l, "W")==0 || strcmp(train_direction_l, "w")==0){
+         direction = "West";    
      }else{
-        printf("Train %2ld is ready to go East\n", (intptr_t)index);
         direction = "East";
      }
-     
+     printf("Train %2d is ready to go %s\n", index_l, direction);
+
+     //add to queue - choose which train to use enqueue here OR don't enqueue here but in main()
+     enqueue(index_l);
+
+     pthread_mutex_lock(&count_mutex); /* Locks code below */
      load_done = 1;
-     pthread_cond_signal(&count_cond); /* Signals load time is finished */
+     pthread_cond_broadcast(&count_cond); /* Signals load time is finished */
      pthread_mutex_unlock(&count_mutex); /* Unlocks code for other threads */
-
-     printf("00:00:0%.1f ", new_time);
-     printf("Train %2ld is ON the main track going %4s\n", (intptr_t)index, direction);
-
-   pthread_mutex_lock(&mutex_cond3);
-   while(load_done2==0){
-     pthread_cond_wait(&count_cond2, &mutex_cond3);
-   }
-
-   pthread_mutex_unlock(&mutex_cond3);
-   load_done2 = 0;
-
-    double cross_time = ((double)train_data[(intptr_t)index].cross_time) / 10;
-    usleep((useconds_t)(cross_time * 1000000));
-
-    printf("00:00:0%.1f ", new_time);
-    printf("Train %2ld is OFF the main track after going %4s\n",(intptr_t)index, direction);
   
     pthread_exit(NULL); 
 
@@ -76,26 +93,45 @@ void *train_departs(void *index){
     /*
      * Function: train_departs
      *
-     * Parameters: 
-     *      index: the number of the train to depart
-     *
      */
+     int t_ind = (intptr_t)index;
 
-     pthread_mutex_lock(&mutex_cond);
-     while(load_done==0){
-        pthread_cond_wait(&count_cond, &mutex_cond);
-     }
+        /* Output train on track */
+        char *t_dir = train_data[t_ind].train_direction;
+        int load_time_l = train_data[t_ind].load_time;
+        int cross_time_l = train_data[t_ind].cross_time;
 
-     load_done2 = 1;
-     pthread_cond_broadcast(&count_cond2);
-     pthread_mutex_unlock(&mutex_cond);
-     load_done = 0;
+        char *dir;
+        if(strcmp(t_dir, "W")==0 || strcmp(t_dir, "w")==0){
+            dir = "West";    
+        }else{
+           dir = "East";
+        }
 
-     pthread_exit(NULL); 
+        double new_time = (double)(load_time_l)/10;
+        //double new_time = curr_time;
+        double new_cross_time = (double)(cross_time_l)/10;
+        double time_l = new_cross_time + new_time;
+        useconds_t load_time2 = (useconds_t)(new_cross_time * 1000000);
+
+        pthread_mutex_lock(&count_mutex);
+
+        /* Output that train is on main track */
+        printf("00:00:0%.1f Train %2d is ON the main track going %4s\n", new_time, t_ind, dir);
+
+        usleep(load_time2); /* Loads time */
+
+        /* Output that train is OFF the main track */
+        printf("00:00:0%.1f Train %2d is OFF the main track after going %4s\n", time_l, t_ind, dir);
+
+        pthread_mutex_unlock(&count_mutex);
+    
+    pthread_exit(NULL); 
 }
 
 int main(int argc, char **argv){ 
     pthread_t threads[75];
+    pthread_t thread2;
 
     pthread_mutex_init(&count_mutex, NULL); /* Initializes mutex */
     pthread_mutex_init(&mutex_cond, NULL);
@@ -120,13 +156,31 @@ int main(int argc, char **argv){
         pthread_create(&threads[i], NULL, loading_time, (void *)(intptr_t)i); /* Loads the trains and decides which train to enter the main track */
     }
 
-    for(int i = 0; i < num_trains; i++){
-        pthread_create(&threads[i], NULL, train_departs, (void *)(intptr_t)i); /* Prints out that train has crossed */
+    for(int i = 0; i < num_trains; i++){ /* Waits until train is ready to cross */
+        
+        pthread_mutex_lock(&count_mutex);
+
+        while(load_done == 0){
+            pthread_cond_wait(&count_cond,&count_mutex);
+        }
+
+        int j = dequeue();
+        pthread_create(&thread2, NULL, train_departs, (void*)(intptr_t)j);
+
+        pthread_mutex_unlock(&count_mutex);
+
+        pthread_mutex_lock(&count_mutex);
+        load_done = 0;
+        pthread_cond_broadcast(&count_cond); /* Signal next train */
+        pthread_mutex_unlock(&count_mutex);
+
     }
 
     for(int i=0; i< num_trains; i++){
         pthread_join(threads[i], NULL); /* Wait for threads to all complete */
     }
+
+    pthread_join(thread2, NULL);
     
     pthread_mutex_destroy(&count_mutex);
     pthread_mutex_destroy(&mutex_cond);
